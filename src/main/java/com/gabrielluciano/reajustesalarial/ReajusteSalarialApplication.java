ee0e88cf1a732f7c1ff6850c82b35a94974fc9d9
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-03-12 18:11:24 -0300

    chore: initial commit

---
+++ src/main/java/com/gabrielluciano/reajustesalarial/ReajusteSalarialApplication.java
@@ -0,0 +1,12 @@
+package com.gabrielluciano.reajustesalarial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReajusteSalarialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReajusteSalarialApplication.class, args);
+    }
+}
