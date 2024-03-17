aa2e439470f35ca67af60fbfb7ff7190a1b6678e
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-03-17 15:59:33 -0300

    feat: add cpf validation and integration tests

--- src/main/java/com/gabrielluciano/reajustesalarial/controllers/ReajusteSalarialController.java
+++ src/main/java/com/gabrielluciano/reajustesalarial/controllers/ReajusteSalarialController.java
@@ -8,12 +8,14 @@ import com.gabrielluciano.reajustesalarial.services.ReajusteSalarialService;
 import com.gabrielluciano.reajustesalarial.util.constraintvalidators.CpfConstraint;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/reajustesalarial")
+@Validated
 public class ReajusteSalarialController {
 
     private final ReajusteSalarialService service;
