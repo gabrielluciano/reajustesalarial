960fd5ed89f5d1ed03815e8021005fe5fdd68387
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-03-17 12:09:21 -0300

    fix: error message

--- src/main/java/com/gabrielluciano/reajustesalarial/exceptions/SalarioAlreadyReajustadoException.java
+++ src/main/java/com/gabrielluciano/reajustesalarial/exceptions/SalarioAlreadyReajustadoException.java
@@ -2,6 +2,6 @@ package com.gabrielluciano.reajustesalarial.exceptions;
 
 public class SalarioAlreadyReajustadoException extends RuntimeException {
     public SalarioAlreadyReajustadoException(String cpf) {
-        super(String.format("Salário do usuário de '%s' já foi reajustado", cpf));
+        super(String.format("Salário do usuário de cpf '%s' já foi reajustado", cpf));
     }
 }
